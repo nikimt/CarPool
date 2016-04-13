129c6110ee8050cd9bf54038b4b3b0c8ad6fb3d4
mslaughter <melissaslaughter@me.com>
2016-04-13 17:02:44 -0400

    not sure what i did, but i think it was maybe phone numbers

--- members/members.js
+++ members/members.js
@@ -109,7 +109,7 @@ var load_members = function () {
             id: 1,
             name: 'Jane Style', 
             img_src: '../images/hedgehog.jpg' ,
-            phone_number: '123-456-7890',
+            phone_number: '306-477-8834',
             email_address: 'janestyle@email.com',
             child_name: 'Larry Style',
             home_address: '123 Some Street, FL 12345'
@@ -118,7 +118,7 @@ var load_members = function () {
             id: 2,
             name: 'Cristie Clark',
             img_src: '../images/hedgehog4.jpg',
-            phone_number: '123-456-7890',
+            phone_number: '444-019-3378',
             email_address: 'cclark@mydomain.com',
             child_name: 'Marcus Clark',
             home_address: '123 Some Street, FL 12345'
@@ -127,16 +127,16 @@ var load_members = function () {
             id: 3,
             name: 'Joanne Li',
             img_src: '../images/hedgehog5.jpg',
-            phone_number: '123-456-7890',
+            phone_number: '431-761-6631',
             email_address: 'jli761@gmail.com',
             child_name: 'Herman Li',
-            home_address: '123 Some Street, FL 12345'
+            home_address: '4 Westbrook Lane, Miami, FL 12345'
         },
         {
             id: 4,
             name: 'Leslie Lemmelson',
             img_src: '../images/hedgehog6.jpg',
-            phone_number: '123-456-7890',
+            phone_number: '444-299-7366',
             email_address: 'leslemmelson@lemmelsonlabs.com',
             child_name: 'Harold Lemmelson',
             home_address: '123 Some Street, FL 12345'
@@ -145,7 +145,7 @@ var load_members = function () {
             id: 5,
             name: 'Amy Lewis',
             img_src: '../images/hedgehog7.jpg',
-            phone_number: '123-456-7890',
+            phone_number: '444-613-7546',
             email_address: 'iamamy@email.com',
             child_name: 'Abigail Lewis',
             home_address: '123 Some Street, FL 12345'
@@ -154,7 +154,7 @@ var load_members = function () {
             id: 6,
             name: 'Jay Z.',
             img_src: '../images/hedgehog8.jpg',
-            phone_number: '123-456-7890',
+            phone_number: '305-999-1200',
             email_address: 'jayz@boss.com',
             child_name: 'Blue Ivy Carter',
             home_address: '123 Some Street, FL 12345'
@@ -163,7 +163,7 @@ var load_members = function () {
             id: 7,
             name: 'Adam Smith',
             img_src: '../images/hedgehog9.jpg',
-            phone_number: '123-456-7890',
+            phone_number: '587-366-0240',
             email_address: 'atleastimnotjohn@generic.com',
             child_name: 'Ronald Smith',
             home_address: '123 Some Street, FL 12345'
@@ -172,7 +172,7 @@ var load_members = function () {
             id: 8,
             name: 'Jodie Walker',
             img_src: '../images/hedgehog10.jpg',
-            phone_number: '123-456-7890',
+            phone_number: '587-787-3209',
             email_address: 'jodiew2k16@email.com',
             child_name: 'Jamie Walker',
             home_address: '123 Some Street, FL 12345'
@@ -181,7 +181,7 @@ var load_members = function () {
             id: 9,
             name: 'Elizabeth Wright',
             img_src: '../images/hedgehog11.jpg',
-            phone_number: '123-456-7890',
+            phone_number: '587-335-9011',
             email_address: 'ewright@weright.com',
             child_name: 'Joe Wright',
             home_address: '123 Some Street, FL 12345'
@@ -190,7 +190,7 @@ var load_members = function () {
             id: 10,
             name: 'Georgia Markus',
             img_src: '../images/hedgehog12.jpg',
-            phone_number: '123-456-7890',
+            phone_number: '444-998-9870',
             email_address: 'georgia@florida.com',
             child_name: 'Virginia Markus',
             home_address: '123 Some Street, FL 12345'
@@ -199,7 +199,7 @@ var load_members = function () {
             id: 11,
             name: 'Penelope Rose',
             img_src: '../images/hedgehog13.jpg',
-            phone_number: '123-456-7890',
+            phone_number: '306-203-4487',
             email_address: 'penelope.rose@flowernames.com',
             child_name: 'Summer Rose',
             home_address: '123 Some Street, FL 12345'
@@ -208,7 +208,7 @@ var load_members = function () {
             id: 12,
             name: 'Tanya West',
             img_src: '../images/hedgehog14.jpg',
-            phone_number: '123-456-7890',
+            phone_number: '444-809-3267',
             email_address: 'tanyawest@cardinal.com',
             child_name: 'Kanye West',
             home_address: '123 Some Street, FL 12345'
@@ -217,7 +217,7 @@ var load_members = function () {
             id: 13,
             name: 'Kim Stephens',
             img_src: '../images/hedgehog15.jpg',
-            phone_number: '123-456-7890',
+            phone_number: '987-370-0909',
             email_address: 'kim@email.com',
             child_name: 'Tim Stephens',
             home_address: '123 Some Street, FL 12345'
@@ -226,7 +226,7 @@ var load_members = function () {
             id: 14,
             name: 'Karen Young',
             img_src: '../images/hedgehog16.jpg',
-            phone_number: '123-456-7890',
+            phone_number: '444-338-5570',
             email_address: 'karen@fountainofyouth.com',
             child_name: 'Wally Young',
             home_address: '123 Some Street, FL 12345'
@@ -235,7 +235,7 @@ var load_members = function () {
             id: 15,
             name: 'Olga Rasmusen',
             img_src: '../images/hedgehog17.jpg',
-            phone_number: '123-456-7890',
+            phone_number: '306-112-0956',
             email_address: 'olgathewitch@email.com',
             child_name: 'Steve Rasmusen',
             home_address: '123 Some Street, FL 12345'
@@ -244,7 +244,7 @@ var load_members = function () {
             id: 16,
             name: 'Clark Simpson',
             img_src: '../images/cat.jpg' ,
-            phone_number: '123-456-7890',
+            phone_number: '948-129-3094',
             email_address: 'iambetterthanbart@simpsons.com',
             child_name: 'Bart Simpson',
             home_address: '123 Some Street, FL 12345'
