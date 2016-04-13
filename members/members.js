c9f388d1c14aa8b7b6b564a53c8bb98b7d7f59b7
mslaughter <melissaslaughter@me.com>
2016-04-13 14:26:27 -0400

    added modals so that things popup on members page

--- members/members.js
+++ members/members.js
@@ -1,31 +1,20 @@
 $(document).ready(function() {
-    var members_list = [
-        { name: 'Jane Style', img_src: '../images/hedgehog.jpg' },
-        { name: 'Cristie Clark', img_src: '../images/hedgehog4.jpg' },
-        { name: 'Joanne Li', img_src: '../images/hedgehog5.jpg' },
-        { name: 'Leslie Lemmelson', img_src: '../images/hedgehog6.jpg' },
-        { name: 'Amy Lewis', img_src: '../images/hedgehog7.jpg' },
-        { name: 'Jay Z.', img_src: '../images/hedgehog8.jpg' },
-        { name: 'Adam Smith', img_src: '../images/hedgehog9.jpg' },
-        { name: 'Jodie Walker', img_src: '../images/hedgehog10.jpg' },
-        { name: 'Ellie Wright', img_src: '../images/hedgehog11.jpg' },
-        { name: 'Georgia Markus', img_src: '../images/hedgehog12.jpg' },
-        { name: 'Penelope Rose', img_src: '../images/hedgehog13.jpg' },
-        { name: 'Tanya West', img_src: '../images/hedgehog14.jpg' },
-        { name: 'Kim Stephens', img_src: '../images/hedgehog15.jpg' },
-        { name: 'Karen Young', img_src: '../images/hedgehog16.jpg' },
-        { name: 'Olga Rasmusen', img_src: '../images/hedgehog17.jpg' },
-        { name: 'Clark Simpson', img_src: '../images/cat.jpg' }
-    ];
+    var members_list = load_members();
 
     generate_cards(members_list);
+    initialize_modals();
 
     $('#search-bar').keyup(function(evt) {
         var search_val = $(this).val();
-        console.log(search_val);
         var visible_members = get_visible_members(members_list, search_val);
-        console.log(visible_members);
         update_cards(visible_members);
+    });
+
+    $('#members-list .ui.card').click(function(evt) {
+        var id = $(this).attr('id');
+        id = id.slice('card-'.length, id.length);
+        var modal_id = "modal-" + id;
+        $('#' + modal_id).modal('show');
     });
 
 });
@@ -64,10 +53,11 @@ var update_cards = function(visible_members) {
 
 var generate_cards = function(members_list) {
     var cards_html = "";
+    var modal_html = "";
     for (var i = 0; i < members_list.length; i++) {
         cards_html += 
             "<div class='column'>" +
-                "<div class='ui fluid card'>" +
+                "<div class='ui fluid card' id='card-" + members_list[i].id + "'>" +
                     "<div class='image'>" +
                         "<img src=" + members_list[i].img_src + ">" +
                     "</div>" +
@@ -77,7 +67,187 @@ var generate_cards = function(members_list) {
                 "</div>" +
             "</div>"
         ;
+        modal_html +=
+            "<div class='ui modal members-modal' id='modal-" + members_list[i].id + "'>" +
+                "<i class='close icon'></i>" +
+                "<div class='header'>" + members_list[i].name + "</div>" +
+                "<div class='image content'>" +
+                    "<div class='image'>" +
+                        "<img src=" + members_list[i].img_src + ">" +
+                    "</div>" +
+                    "<div class='description ui grid two column'>" +
+                        "<div class='column'>" +
+                            "<h5 class='ui header'>Phone Number</h5>" +
+                            "<p>" + members_list[i].phone_number + "</p>" +
+                            "<h5 class='ui header'>Email Address</h5>" +
+                            "<p>" + members_list[i].email_address + "</p>" +
+                        "</div>" +
+                        "<div class='column'>" +
+                            "<h5 class='ui header'>Child's Name</h5>" +
+                            "<p>" + members_list[i].child_name + "</p>" +
+                            "<h5 class='ui header'>Home Address</h5>" +
+                            "<p>" + members_list[i].home_address + "</p>" +
+                        "</div>"
+                    "</div>" +
+                "</div>" + 
+            "</div>"
+        ;
+        console.log(modal_html);
     }
+    cards_html += modal_html;
     var menu_element = document.getElementById("members-list");
     menu_element.innerHTML = cards_html;
+}
+
+var initialize_modals = function() {
+    $('.ui.modal').modal();
+}
+
+var load_members = function () {
+    return [
+        {
+            id: 1,
+            name: 'Jane Style', 
+            img_src: '../images/hedgehog.jpg' ,
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Larry Style',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 2,
+            name: 'Cristie Clark',
+            img_src: '../images/hedgehog4.jpg',
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Marcus Clark',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 3,
+            name: 'Joanne Li',
+            img_src: '../images/hedgehog5.jpg',
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Herman Li',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 4,
+            name: 'Leslie Lemmelson',
+            img_src: '../images/hedgehog6.jpg',
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Harold Lemmelson',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 5,
+            name: 'Amy Lewis',
+            img_src: '../images/hedgehog7.jpg',
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Abigail Lewis',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 6,
+            name: 'Jay Z.',
+            img_src: '../images/hedgehog8.jpg',
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Blue Ivy Carter',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 7,
+            name: 'Adam Smith',
+            img_src: '../images/hedgehog9.jpg',
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Ronald Smith',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 8,
+            name: 'Jodie Walker',
+            img_src: '../images/hedgehog10.jpg',
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Jamie Walker',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 9,
+            name: 'Elizabeth Wright',
+            img_src: '../images/hedgehog11.jpg',
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Joe Wright',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 10,
+            name: 'Georgia Markus',
+            img_src: '../images/hedgehog12.jpg',
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Virginia Markus',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 11,
+            name: 'Penelope Rose',
+            img_src: '../images/hedgehog13.jpg',
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Summer Rose',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 12,
+            name: 'Tanya West',
+            img_src: '../images/hedgehog14.jpg',
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Kanye West',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 13,
+            name: 'Kim Stephens',
+            img_src: '../images/hedgehog15.jpg',
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Tim Stephens',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 14,
+            name: 'Karen Young',
+            img_src: '../images/hedgehog16.jpg',
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Wally Young',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 15,
+            name: 'Olga Rasmusen',
+            img_src: '../images/hedgehog17.jpg',
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Steve Rasmusen',
+            home_address: '123 Some Street, FL 12345'
+        },
+        {
+            id: 16,
+            name: 'Clark Simpson',
+            img_src: '../images/cat.jpg' ,
+            phone_number: '123-456-7890',
+            email_address: 'example@email.com',
+            child_name: 'Bart Simpson',
+            home_address: '123 Some Street, FL 12345'
+        }
+    ];
 }
