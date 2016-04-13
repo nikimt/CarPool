de54969028c5914a38ea5e87407f28b83fb1e359
Jessica Li <jesslli@mit.edu>
2016-04-13 06:59:50 -0400

    calendar and popup

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,44 +1,92 @@
 $(document).ready(function() {
     var today = new Date();
     var first = new Date(today.getFullYear(), today.getMonth(), 1);
-    console.log(first.getDay());
-    console.log(today.getDate());
-    console.log(today.getDay());
-    load_dates(first);
+    var total_days = new Date(today.getFullYear(), today.getMonth()+1, 0).getDate();
+    load_dates(first, total_days);
+
+    // $('.selectable').click(function(evt) {
+    //     // var popup_html = 
+    //     // "<div class='ui popup'>" +
+    //     //   "<div class='header'>User Rating</div>" +
+    //     //   "<div class='ui star rating' data-rating='3'></div>" +
+    //     // "</div>";
+    //     // $('#calendar_page').append(popup_html);
+    //     // console.log(popup_html);
+    //     // $('.selectable')
+    //     //     .popup({
+    //     //         popup: '.special.popup'
+    //     //     })
+    //     // ;
+    // });
+    $('.selectable')
+        .popup({
+            on: 'click',
+            popup: '.special.popup',
+            // title: 'My favorite dog',
+            // content: 'My favorite dog would like other dogs as much as themselves',
+        })
+    ;
 });
 
-var load_dates = function(first) {
-    var firstWeekday = first.getDay();
-    calendar_html = "<tr class='calendar-row'>";
-    for (i = 0; i < firstWeekday; i++) {
-        calendar_html += "<td class='selectable'>" +
-            "<a href='#'>1</a>" +
-        "</td>" +
+var load_dates = function(first, total_days) {
+    var currentDay = 1;
+    currentDay = load_first_row(first, currentDay);
+    var row_html = "";
+
+    var col = 0;
+    while (currentDay <= total_days) {
+        if (col == 0) {
+            row_html += "<tr class='calendar-row'>";
+            col += 1;
+        }
+        else if (col > 0 && col <= 7) {
+            row_html +=
+            "<td class='selectable'>" +
+                currentDay +
+            "</td>";
+            currentDay += 1;
+            col += 1;
+        }
+        if (col == 8) {
+            row_html += "</tr";
+            col = 0;
+            $('#calendar_display').append(row_html);
+            row_html = "";
+        }
     }
+    if (col != 0) {
+        for (j = col; j < 7; j++) {
+            row_html +=
+             "<td class='selectable'>" +
+            "</td>";
+        }
+        row_html += "</tr>";
+        $('#calendar_display').append(row_html);
+    }
+    
+}
 
+var load_first_row = function(first, currentDay) {
+    var firstWeekday = first.getDay();
 
+    var calendar_html = "<tr class='calendar-row'>";
+    var last_month_date = 27;
+    for (i = 0; i < firstWeekday; i++) {
+        calendar_html +=
         "<td class='selectable'>" +
-            "<a href='#'>1</a>" +
-        "</td>" +
-        "<td class='selectable'>" +
-            "<a href='#'>2</a>" +
-        "</td>" +
-        "<td class='selectable'>" +
-            "<a href='#'>3</a>" +
-        "</td>" +
-        "<td class='selectable'>" +
-            "<a href='#'>4</a>" +
-        "</td>" +
-        "<td class='selectable'>" +
-            "<a href='#'>5</a>" +
-        "</td>" +
-        "<td class='selectable'>" +
-            "<a href='#'>6</a>" +
-        "</td>" +
-        "<td class='selectable'>" +
-            "<a href='#'>7</a>" +
-        "</td>" +
-
-    calendar_html += "</tr>";
+            "<div class='last-month-date-number'>" + last_month_date + "</div>" +
+            // "<div class='icons'>" + "hi" + "</div>" +
+        "</td>";
+        last_month_date += 1;
+    }
+    for (j = firstWeekday; j < 7; j++) {
+        calendar_html +=
+         "<td class='selectable'>" +
+            currentDay +
+        "</td>";
+        currentDay += 1;
+    }
+    calendar_html += "</tr";
     $('#calendar_display').append(calendar_html);
+    return currentDay;
 }
