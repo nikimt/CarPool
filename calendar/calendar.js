6790d8d6e0d2c4e421b27d0ba869e8e705caae82
Jessica Li <jesslli@mit.edu>
2016-05-01 13:44:29 -0400

    highlighted practice days

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
     var today = new Date();
+    // var today = new Date(2016, 3, 17)
     var first = new Date(today.getFullYear(), today.getMonth(), 1);
     var total_days = new Date(today.getFullYear(), today.getMonth()+1, 0).getDate();
-    load_dates(first, total_days);
+    load_dates(today, first, total_days);
 
     var clear_inputs = function() {
         console.log('clearing inputs');
@@ -43,9 +44,9 @@ $(document).ready(function() {
     $('#this-18').append("<i class='big shipping icon'></i>")
 });
 
-var load_dates = function(first, total_days) {
+var load_dates = function(today, first, total_days) {
     var currentDay = 1;
-    currentDay = load_first_row(first, currentDay);
+    currentDay = load_first_row(today, first, currentDay);
     var row_html = "";
 
     var col = 0;
@@ -55,10 +56,14 @@ var load_dates = function(first, total_days) {
             col += 1;
         }
         else if (col > 0 && col <= 7) {
-            row_html +=
-            "<td class='selectable' id=this-" + currentDay + ">" +
-                "<div class='date-number'>" + currentDay + "</div" +
-            "</td>";
+            if (col == 2 || col == 3 || col == 6) {
+                row_html += "<td class='selectable practice' "
+            } else {
+                row_html += "<td class='selectable' "
+            }
+            row_html += "id=this-" + currentDay + ">" +
+                        "<div class='date-number'>" + currentDay + "</div" +
+                        "</td>";
             currentDay += 1;
             col += 1;
         }
@@ -69,11 +74,18 @@ var load_dates = function(first, total_days) {
             row_html = "";
         }
     }
+    next_month_date = 1;
     if (col != 0) {
-        for (j = col; j < 7; j++) {
-            row_html +=
-             "<td class='selectable'>" +
-            "</td>";
+        for (j = col; j < 8; j++) {
+            if (j == 2 || j == 3 || j == 6) {
+                row_html += "<td class='selectable practice'>"
+            } else {
+                row_html += "<td class='selectable'>"
+            }
+            row_html += "<div class='last-month-date-number'>" + next_month_date + "</div>" +
+                "</td>";
+            next_month_date += 1;
+
         }
         row_html += "</tr>";
         $('#calendar_display').append(row_html);
@@ -81,23 +93,30 @@ var load_dates = function(first, total_days) {
     
 }
 
-var load_first_row = function(first, currentDay) {
+var load_first_row = function(today, first, currentDay) {
     var firstWeekday = first.getDay();
 
     var calendar_html = "<tr class='calendar-row'>";
     var last_month_date = 27;
     for (i = 0; i < firstWeekday; i++) {
-        calendar_html +=
-        "<td class='selectable' id='prev-" + last_month_date + "'>" +
-            "<div class='last-month-date-number'>" + last_month_date + "</div>" +
-        "</td>";
+        if (i == 1 || i == 2 || i == 5) {
+            calendar_html += "<td class='selectable practice' "
+        } else {
+            calendar_html += "<td class='selectable' "
+        }
+        calendar_html += "id='prev-" + last_month_date + "'>" +
+                            "<div class='last-month-date-number'>" + last_month_date + "</div>" +
+                        "</td>";
         last_month_date += 1;
     }
     for (j = firstWeekday; j < 7; j++) {
-        calendar_html +=
-         "<td class='selectable' id='this-" + currentDay + "'>" +
-            currentDay +
-        "</td>";
+        if (j == 1 || j == 2 || j == 5) {
+            calendar_html += "<td class='selectable practice' "
+        }
+        else {
+            calendar_html += "<td class='selectable' "
+        }
+        calendar_html += "id='this-" + currentDay + "'>" + currentDay + "</td>";
         currentDay += 1;
     }
     calendar_html += "</tr";
