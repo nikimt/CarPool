3dbb7b05e91f9b1c54e29230c2aecd4284c9a698
mslaughter <melissaslaughter@me.com>
2016-05-01 23:18:08 -0400

    added search to addteams page

--- addteam/addteam.js
+++ addteam/addteam.js
@@ -9,7 +9,44 @@ $(document).ready(function() {
         localStorage.setItem('teams', JSON.stringify(current_teams));
     // }
     generate_team_cards(current_teams);
+
+    $('#search-bar').keyup(function(evt) {
+        var search_val = $(this).val();
+        var visible_teams = get_visible_teams(current_teams, search_val);
+        update_cards(visible_teams);
+    })
 });
+
+var get_visible_teams = function(teams_list, search_val) {
+    var visible_teams = [];
+    for (var i = 0; i < teams_list.length; i++) {
+        var team = teams_list[i];
+        if (team.name.toLowerCase().indexOf(search_val.toLowerCase()) != -1) {
+            visible_teams.push(team);
+        }
+    }
+    return visible_teams;
+}
+
+var update_cards = function(visible_teams) {
+    var cards = $('#team-list .card');
+    for (var i = 0; i < cards.length; i++) {
+        var card = $(cards[i]);
+        var content = $($(card.children()[0]).children()[0]); 
+        var is_hide = true;
+        for (var j = 0; j < visible_teams.length; j++) {
+            if (content.text() == visible_teams[j].name) {
+                is_hide = false;
+                break;
+            }
+        }
+        if (is_hide) {
+            card.css("display", "none");
+        } else {
+            card.css("display", "");
+        }
+    }
+}
 
 var generate_team_cards = function(teams) {
     var cards_html = "";
@@ -23,9 +60,6 @@ var generate_team_cards = function(teams) {
         for (var j = 0; j < teams[i].times.length; j++) {
             times_html += "<p>" + teams[i].times[j] + "</p>"
         }
-        console.log(teams[i]);
-        console.log(teams[i].name);
-        console.log(teams[i].extra);
 
         cards_html +=
             "<div class='card'>" + 
